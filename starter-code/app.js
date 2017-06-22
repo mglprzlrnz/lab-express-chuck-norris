32f69f01041f7421a67aa258c326b0a73c338910
Tuyet-Trinh thi Vu <mglprzlrnz@gmail.com>
2017-06-22 21:39:35 +0200

    Iteration 1 completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,35 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+
+});
+app.get('/random', (req, res, next) => {
+  // Retrieve a random chuck joke
+  client.getRandomJoke()
+    .then((response) => {
+      // res.render ("random")
+      res.render ("random", response);
+    }).catch((err) => {
+      res.send ("There is an error");
+    });
+});
+app.get('/categories', (req, res, next) => {
+  client.getJokeCategories()
+    .then((response)=>  {
+      res.render ("categories", {categories: response});
+    })
+    .catch((err)=> {
+      res.send ("There is an error");
+    });
+});
+app.get('/search', (req, res, next) => {
+
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!')
+});
